7f0b6978d37a4151615f64a5e64310f4a879a537
sabelogumede <iamsabelogumede@gmail.com>
2019-09-24 12:11:00 +0200

    lint fix in shopping.reducer.ts

--- src/app/store/reducers/shopping.reducer.ts
+++ src/app/store/reducers/shopping.reducer.ts
@@ -1,9 +1,9 @@
-import { ShoppingItem } from "../models/shopping-item.model";
+import { ShoppingItem } from '../models/shopping-item.model';
 import { ShoppingAction, ShoppingActionTypes } from '../actions/shopping.actions';
 // initialize a state with new data
 const initialState: Array<ShoppingItem> = [
   {
-    id: "1775935f-36fd-467e-a667-09f95b917f6d",
+    id: '1775935f-36fd-467e-a667-09f95b917f6d',
     name: 'Diet coke',
   }
 ];
