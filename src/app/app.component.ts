d0396a73318198bf61910c188c99f4977e609f2a
sabelogumede <iamsabelogumede@gmail.com>
2019-09-24 14:42:18 +0200

    added a item input and state update functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AppState } from './store/models/app-state.model';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+import { AppState } from './store/models/app-state.model';
 import { ShoppingItem } from './store/models/shopping-item.model';
+import { AddItemAction } from './store/actions/shopping.actions';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +15,26 @@ import { ShoppingItem } from './store/models/shopping-item.model';
 export class AppComponent implements OnInit {
   shoppingItems$: Observable<Array<ShoppingItem>>;
   constructor(private store: Store<AppState>) {}
+  // new-ShoppingItem instant initialization with empty input fields
+  newShoppingItem: ShoppingItem = { id: '', name: ''};
 
   ngOnInit(): void {
     this.shoppingItems$ = this.store.select(store => store.shopping);
+
+    // after 2sec addItem will add item
+    // setTimeout(() => this.addItem(), 2000);
+  }
+
+  // addItem function dispatches a new action='AddItemAction to the store with a payload of items
+  // using addItemAcction function import from actions
+  addItem() {
+    this.newShoppingItem.id = uuid();
+    // firing the AddItemAction in actions taking inn a payload of newShoppingItem inside shopping-reducer
+    // in shopping-reducer, the action type is checked in the switch statement
+    // and return a new version or ...
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+    // after it has been dispatched set to blank
+    this.newShoppingItem = { id: '', name: ''};
   }
 
 }
--- src/app/store/reducers/shopping.reducer.ts
+++ src/app/store/reducers/shopping.reducer.ts
@@ -4,13 +4,18 @@ import { ShoppingAction, ShoppingActionTypes } from '../actions/shopping.actions
 const initialState: Array<ShoppingItem> = [
   {
     id: '1775935f-36fd-467e-a667-09f95b917f6d',
-    name: 'Diet coke',
+    name: 'Fanta',
+  },
+  {
+    id: '1775935f-36fd-467e-a667-19f95b917f6d',
+    name: 'Diet Coke',
   }
 ];
 
 // export a function called ShoppingReducer
 // it takes in to items "state" + "action"
 export function ShoppingReducer(
+  // state is set to the initialState above
   state: Array<ShoppingItem> = initialState,
   action: ShoppingAction) {
     switch (action.type) {
